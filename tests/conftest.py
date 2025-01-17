181f2cf699909a7698edbd0da9ea23b0c1478492
pushkarop <pushkarsingh4349@gmail.com>
2025-01-18 01:09:27 +0530

    test: update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ pytest.register_assert_rewrite('tests')
 
 def pytest_configure(config):
     config.addinivalue_line(
-        "asyncio_default_fixture_loop_scope",
-        "function"
+        "asyncio_mode",
+        "auto"
     ) 
